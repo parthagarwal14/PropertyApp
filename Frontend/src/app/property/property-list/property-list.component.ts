08de92583b5af92d8771618700a30f0e9c1cc3c4
Parth <parthagarwal232@gmail.com>
2024-04-29 13:26:24 +0530

    modified:   Frontend/src/app/app.component.html 	modified:   Frontend/src/app/property/property-card/property-card.component.html 	modified:   Frontend/src/app/property/property-card/property-card.component.ts 	modified:   Frontend/src/app/property/property-list/property-list.component.html 	modified:   Frontend/src/app/property/property-list/property-list.component.ts

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class PropertyListComponent {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"White House",
+      "Type":"House",
+      "Price":20000
+    },
+    {
+      "Id":3,
+      "Name":"Reliance House",
+      "Type":"House",
+      "Price":18000
+    },
+    {
+      "Id":4,
+      "Name":"Macro House",
+      "Type":"House",
+      "Price":15000
+    },
+    {
+      "Id":5,
+      "Name":"Erose Villa",
+      "Type":"Villa",
+      "Price":10000
+    },
+    {
+      "Id":6,
+      "Name":"BPTP",
+      "Type":"Villas",
+      "Price":22000
+    },
+  ]
 }
